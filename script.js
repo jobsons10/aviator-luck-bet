b6f44d6837e8995e2f3deff70779fc91ca86b17a
jobsons10 <jobsonsobrinho@grupootg.com>
2023-01-05 16:06:29 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Get the button and value elements
+var button = document.getElementById("button");
+var value = document.getElementById("value");
+
+// Add an event listener to the button
+const generate = function() {
+  // Generate a random number between 0 and 1
+  var randomNumber = Math.random();
+
+  // Use an if statement to determine which range to use
+  if (randomNumber < 0.8) {
+    // Generate a random value between 1.05 and 1.20
+    var randomValue = (Math.random() * (1.20 - 1.05) + 1.05).toFixed(2);
+  } else {
+    // Generate a random value between 1.20 and 1.80
+    var randomValue = (Math.random() * (1.80 - 1.20) + 1.20).toFixed(2);
+  }
+
+  // Update the value element with the new value
+  value.textContent = randomValue;
+};

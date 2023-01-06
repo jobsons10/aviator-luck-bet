8e2a9e620e6181362ba8908b89ba78522520249b
jobsons10 <jobsonsobrinho@grupootg.com>
2023-01-05 20:31:15 -0400

    button

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const generate = function() {
     setTimeout(function() {
         loading.classList.add("display-none");
         value.classList.remove("display-none");
-    }, 4000)
+    }, 3000)
 
     // Generate a random number between 0 and 1
     var randomNumber = Math.random();
